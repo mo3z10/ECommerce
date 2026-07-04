893ba21054ad91e3f4d33c784af43f18f5a37208
mo3z10 <moazyasser070>
2026-07-04 21:27:54 +0300

    edit update order

--- ECommerce.BIL/Services/CartService/CartService.cs
+++ ECommerce.BIL/Services/CartService/CartService.cs
@@ -43,7 +43,7 @@ namespace ECommerce.BIL.Services.CartService
                 var newQuantity = existingItem.Quantity + dto.Quaintity;
                 if (newQuantity > product.QuntityInStock)
                 {
-                    throw new Exception(
+                    throw new InvalidOperationException(
                         $"Quantity isn't available. Only {product.QuntityInStock} from {product.Name}"
                     );
                 }
